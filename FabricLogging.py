ad1556d2390cc35758cd38c111de7d2f3b8fbdee
EmilKofodSejer <emilkofodsejer@gmail.com>
2024-08-08 13:53:04 +0200

    Update FabricLogging.py
    Added default values configuration to include useful information from the Fabric application in your logs.

--- FabricLogging.py
+++ FabricLogging.py
@@ -3,6 +3,7 @@ from pyspark.sql.types import StructType, StructField, StringType, IntegerType, 
 import json
 import csv
 from datetime import datetime
+import sempy.fabric as fabric
 
 CRITICAL = 50
 ERROR = 40
@@ -85,6 +86,7 @@ class CSVHandler:
     def emit(self, record):
         if not self.check_if_log_file_exists(self.full_path):
             mssparkutils.fs.put(self.full_path, self.create_csv_header(record.list_schema())) 
+            mssparkutils.fs.append(self.full_path, """\n""" + ",".join(str(value) for value in record.list_values()), True)
 
         mssparkutils.fs.append(self.full_path, """\n""" + ",".join(str(value) for value in record.list_values()), True)
 
@@ -104,7 +106,7 @@ class CSVHandler:
 
 class FabricLogger():
 
-    def __init__(self, name, level=NOTSET, **kwargs):
+    def __init__(self, name, level=NOTSET, datetime_format='%Y-%m-%d', **kwargs):
         if not isinstance(name, str):
             raise TypeError('A logger name must be a string')
         
@@ -113,7 +115,33 @@ class FabricLogger():
         self.name = name
         self.level = _checkLevel(level)
         self.disabled = False
-        self.handlers = handlers
+        self.datetime_format = datetime_format
+
+        self.default_value_configuration = {
+            "date": False,
+            "workspace_id": False,
+            "workspace_name": False,
+            "notebook_id": False,
+            "notebook_name": False,
+
+        }
+
+        self.set_config(**kwargs)
+
+    def set_config(self, **kwargs):
+        for key, value in kwargs.items(): 
+
+            if key not in self.default_value_configuration:
+                continue
+
+            if value != True:
+                continue
+
+            if not isinstance(value, bool):
+                raise ValueError(f"Invalid configuration option value type: {type(value)}")
+
+            self.default_value_configuration[key] = value    
+
 
     def isEnabledFor(self, level):
         if self.disabled:
@@ -149,16 +177,18 @@ class FabricLogger():
 
     def _log(self, level, msg, **kwargs):
 
-        record = self.makeRecord(self.name, level, msg, **kwargs)
+        record = self.makeRecord(self.name, level, msg, self.default_value_configuration, **kwargs)
         self.handle(record)
 
-    def makeRecord(self, name, level, message, **kwargs):
+    def makeRecord(self, name, level, message, config, **kwargs):
         if kwargs is not None:
             for key in kwargs:
                 if (key in ["name","message","level","asctime"]):
                     raise KeyError(f"Attempt to overwrite {key} in LogRecord")
+                elif (key in ["date", "workspace_id","workspace_name", "notebook_id", "notebook_name"]):
+                    kwargs.remove(key)
 
-        rv = LogRecord(name, level, message)
+        rv = LogRecord(name, level, message, config, **kwargs)
 
         return rv
     
@@ -176,12 +206,29 @@ class FabricLogger():
                                     
 class LogRecord:
 
-    def __init__(self, name, level, message, **kwargs):
+    def __init__(self, name, level, message, config,**kwargs):
         self.name = name
         self.message = message
-        self.levelname = getLevelName(level)
+        self.levelname = getLevelName(level)    
         self.levelnumber = _checkLevel(level)
-        self.kwargs = kwargs
+        self.config = config
+        self.kwargs = kwargs        
+
+        self.active_config = {
+            "date": lambda: datetime.now().strftime(self.datetime_format),
+            "workspace_id": fabric.get_notebook_workspace_id(),
+            "workspace_name": fabric.resolve_workspace_name(),
+            "notebook_id": fabric.get_artifact_id(),
+            "notebook_name": fabric.resolve_item_name(item_id=fabric.get_artifact_id(), type="Notebook")
+
+        }
+
+        self.trim_config()
+
+    def trim_config(self):
+        for key, value in self.config.items():
+            if not value:
+                del self.active_config[key]
 
     def return_as_dict(self):
         LogRecord_as_dict = {
@@ -190,8 +237,8 @@ class LogRecord:
             'levelname': {"value":self.levelname, "dtype":"<class 'str'>"},
             'levelnumber': {"value":self.levelnumber, "dtype":"<class 'int'>"},
         }
+        LogRecord_as_dict.update({str(key): {"value":value, "dtype": str(type(value))} for key, value in self.active_config.items()})
         LogRecord_as_dict.update({str(key):{"value":v, "dtype": str(type(v))} for (key,v) in self.kwargs.items()})
-
         return LogRecord_as_dict
     
     def list_schema(self):
@@ -199,6 +246,3 @@ class LogRecord:
 
     def list_values(self):
         return [d["value"] for d in self.return_as_dict().values()]
-
-
-
